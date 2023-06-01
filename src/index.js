2c0043977eeb5a3a88f143310e350ad03c6f5afb
Ilya Frolov <absolem.dev@gmail.com>
2023-06-01 17:56:26 +0400

    Redux. Part 2. Task 1

--- src/index.js
+++ src/index.js
@@ -1,50 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import { titleChanged, taskDeleted, completeTask } from "./store/task";
+import {
+  titleChanged,
+  taskDeleted,
+  completeTask,
+  loadTasks,
+  getTasks,
+  getTaskLoadingStatus,
+  createTask,
+} from "./store/task";
 import configureStore from "./store/store";
+import { Provider } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getError } from "./store/errors";
 
 const store = configureStore();
 
 const App = (params) => {
-  const [state, setState] = useState(store.getState());
+  const state = useSelector(getTasks());
+  const isLoading = useSelector(getTaskLoadingStatus());
+  const error = useSelector(getError());
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    store.subscribe(() => {
-      setState(store.getState());
-    });
+    dispatch(loadTasks());
   }, []);
 
-  const changeTitle = (taskId) => {
-    store.dispatch(titleChanged(taskId));
-  };
-  const deleteTask = (taskId) => {
-    store.dispatch(taskDeleted(taskId));
-  };
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <>
-      <h1> App</h1>
-      <ul>
-        {state.map((el) => (
-          <li key={el.id}>
-            <p>{el.title}</p>
-            <p> {`Completed: ${el.completed}`}</p>
-            <button onClick={() => store.dispatch(completeTask(el.id))}>
-              Complete
-            </button>
-            <button onClick={() => changeTitle(el.id)}>Change title</button>
-            <button onClick={() => deleteTask(el.id)}>Delete</button>
-            <hr />
-          </li>
-        ))}
-      </ul>
+      <h1>ToDo List</h1>
+      <button onClick={() => dispatch(createTask())} disabled={isLoading}>
+        Add task
+      </button>
+      {!isLoading ? (
+        <ul>
+          {state.map((el) => (
+            <li key={el.id}>
+              <p>{el.title}</p>
+              <p> {`Completed: ${el.completed}`}</p>
+              <button onClick={() => dispatch(completeTask(el.id))}>
+                Complete
+              </button>
+              <button onClick={() => dispatch(titleChanged(el.id))}>
+                Change title
+              </button>
+              <button onClick={() => dispatch(taskDeleted(el.id))}>
+                Delete
+              </button>
+              <hr />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </>
   );
 };
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/store/task.js
+++ src/store/task.js
@@ -1,37 +1,71 @@
 import { createSlice } from "@reduxjs/toolkit";
+import todosService from "../services/todos.service";
+import { setError } from "./errors";
 
-const initialState = [
-  { id: 1, title: "Task 1", completed: false },
-  { id: 2, title: "Task 2", completed: false },
-];
-
-// const update = createAction("task/updated");
-// const remove = createAction("task/removed");
+const initialState = { entities: [], isLoading: true };
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
+    received(state, action) {
+      state.entities = action.payload;
+      state.isLoading = false;
+    },
     update(state, action) {
-      const elementIndex = state.findIndex((el) => el.id === action.payload.id);
-      state[elementIndex] = {
-        ...state[elementIndex],
+      const elementIndex = state.entities.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      state.entities[elementIndex] = {
+        ...state.entities[elementIndex],
         ...action.payload,
       };
     },
+    add(state, action) {
+      state.entities.unshift({ ...action.payload, id: Date.now() });
+      state.isLoading = false;
+    },
     remove(state, action) {
-      return state.filter((el) => el.id !== action.payload.id);
+      state.entities = state.entities.filter(
+        (el) => el.id !== action.payload.id
+      );
+    },
+    taskRequested(state) {
+      state.isLoading = true;
+    },
+    taskRequestFailed(state) {
+      state.isLoading = false;
     },
   },
 });
 
 const { actions, reducer: taskReducer } = taskSlice;
-const { update, remove } = actions;
+const { received, update, add, remove, taskRequested, taskRequestFailed } =
+  actions;
 
-export const completeTask = (id) => (dispatch, getState) => {
+export const loadTasks = () => async (dispatch) => {
+  dispatch(taskRequested());
+  try {
+    const data = await todosService.fetch();
+    dispatch(received(data));
+  } catch (error) {
+    dispatch(taskRequestFailed());
+    dispatch(setError(error.message));
+  }
+};
+export const createTask = () => async (dispatch) => {
+  dispatch(taskRequested());
+  try {
+    const data = await todosService.create();
+    dispatch(add(data));
+  } catch (error) {
+    dispatch(taskRequestFailed());
+    dispatch(setError(error.message));
+  }
+};
+export const completeTask = (id) => (dispatch) => {
   dispatch(update({ id, completed: true }));
 };
-
 export function titleChanged(id) {
   return update({ id, title: `New title for ${id}` });
 }
@@ -39,18 +73,7 @@ export function taskDeleted(id) {
   return remove({ id });
 }
 
-// const taskReducer = createReducer(initialState, (builder) => {
-//   builder
-//     .addCase(update, (state, action) => {
-//       const elementIndex = state.findIndex((el) => el.id === action.payload.id);
-//       state[elementIndex] = {
-//         ...state[elementIndex],
-//         ...action.payload,
-//       };
-//     })
-//     .addCase(remove, (state, action) => {
-//       return state.filter((el) => el.id !== action.payload.id);
-//     });
-// });
+export const getTasks = () => (state) => state.tasks.entities;
+export const getTaskLoadingStatus = () => (state) => state.tasks.isLoading;
 
 export default taskReducer;
